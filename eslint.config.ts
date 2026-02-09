645efe947b4cbd587cbd570c175d2bece0c2ffe7
Benjamin Canac <canacb1@gmail.com>
2026-02-09 14:56:55 +0100

    up

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,7 @@
-import js from '@eslint/js'
 import eslintPluginVue from 'eslint-plugin-vue'
 import ts from 'typescript-eslint'
 
 export default ts.config(
-  js.configs.recommended,
   ...ts.configs.recommended,
   ...eslintPluginVue.configs['flat/recommended'],
   {
